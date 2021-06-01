7226019a99343e10df5a80181ab2350743dd6980
SAMSUNG <SAMSUNG@DESKTOP-IU39QFI>
2021-06-01 11:05:35 +0900

    r1-1

---
+++ src/main/java/com/FRONT/ZzZ1Application.java
@@ -0,0 +1,13 @@
+package com.FRONT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZzZ1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZzZ1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/FRONT/ZzZ1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FRONT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZzZ1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
